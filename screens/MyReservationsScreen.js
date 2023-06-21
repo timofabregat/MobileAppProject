5e0fc39f465c9f6138f70984e85eedecee46f14d
Juan Manuel <manuhermida8@gmail.com>
2023-06-21 12:42:50 -0300

    Se implementa Cancelar Reserva

--- screens/MyReservationsScreen.js
+++ screens/MyReservationsScreen.js
@@ -1,15 +1,39 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text, StyleSheet, TouchableOpacity, FlatList } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity, FlatList, Alert } from 'react-native';
 import { FontAwesome } from '@expo/vector-icons';
 import { useNavigation, NavigationContainer } from '@react-navigation/native';
 import UserService from '../data/UserService';
 import { auth } from '../firebase';
 import AppLoader from './AppLoader';
+import ReservaService from '../data/ReservaService';
 
 const MyReservationsScreen = () => {
   const navigation = useNavigation();
   const [reservas, setReservas] = useState([]);
   const [isLoading, setLoading] = useState(false)
+
+  const handleCancelPress = async (reserva) => {
+    showConfirmationAlert(auth.currentUser.uid, reserva)
+  }
+
+  const showConfirmationAlert = (uid, reserva) => {
+    Alert.alert(
+      'Confirmación de cancelación',
+      '¿Estás seguro que quieres cancelar la reserva?',
+      [
+        { text: 'No', style: 'cancel' },
+        { text: 'Si', onPress: () => handleCancelConfirmed(uid, reserva) },
+      ]
+    );
+  };
+
+  const handleCancelConfirmed = async (uid, reserva) => {
+    setLoading(true)
+    UserService.cancelUserReservation(auth.currentUser.uid, reserva)
+    console.log('volvi')
+    ReservaService.cancelReservation(auth.currentUser.uid, reserva)
+    setLoading(false)
+  };
 
   useEffect(() => {
     const fetchReservations = async () => {
@@ -41,9 +65,12 @@ const MyReservationsScreen = () => {
               <View key={item.id} style={styles.reservaContainer}>
                 <Text style={styles.peluqueria}>{item.peluqueria.name}</Text>
                 <Text style={styles.infoText}>Dirección: {item.peluqueria.direccion}</Text>
-              <Text style={styles.infoText}>Teléfono: {item.phone}</Text>
-              <Text style={styles.infoText}>Fecha: {item.fecha}</Text>
-              <Text style={styles.infoText}>Hora: {item.hora}</Text>
+                <Text style={styles.infoText}>Teléfono: {item.phone}</Text>
+                <Text style={styles.infoText}>Fecha: {item.fecha}</Text>
+                <Text style={styles.infoText}>Hora: {item.hora}</Text>
+                <TouchableOpacity onPress={() => handleCancelPress(item)} style={styles.cancelButton}>
+                  <Text style={styles.cancelButtonText}>Cancelar Reserva</Text>
+                </TouchableOpacity>
               </View>
             }
             showsVerticalScrollIndicator={false}
@@ -77,6 +104,17 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     marginBottom: 8,
     color: 'white'
+  },
+  cancelButton: {
+    backgroundColor: 'red',
+    padding: 10,
+    borderRadius: 5,
+    marginTop: 10,
+  },
+  cancelButtonText: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    textAlign: 'center',
   },
   infoText: {
     fontSize: 16,
